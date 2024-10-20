3e69e421717ebeec68395dd0e2c4beb8f9821d82
Sankalp52 <143029767+Sankalp52@users.noreply.github.com>
2024-10-20 17:02:13 +0530

    Add files via upload

---
+++ AdultOrSeniorCitizenOrChild.java
@@ -0,0 +1,14 @@
+public class AdultOrSeniorCitizenOrChild {
+    public static void main(String[] args) {
+        boolean isSeniorCitizen=false;
+        boolean isAnAdult=true;
+
+        if(isSeniorCitizen){
+            System.out.println("hello senior citizen");
+        } else if (isAnAdult) {
+            System.out.println("hello Adult");
+        }else{
+            System.out.println("hello child");
+        }
+    }
+}
