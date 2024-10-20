98026b63a360d45cd79cc31f2f5557313258b7ec
Sankalp52 <143029767+Sankalp52@users.noreply.github.com>
2024-10-20 17:04:43 +0530

    Add files via upload

---
+++ BitwiseXOR.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class BitwiseXOR {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("enter the first number");
+        int num1=input.nextInt();
+        System.out.println("enter second the number");
+        int num2=input.nextInt();
+        System.out.println("result is:"+(num1^num2));
+    }
+}
