006292018d2aef926936d24eea4146af84fbc104
Sankalp52 <143029767+Sankalp52@users.noreply.github.com>
2024-10-20 17:02:33 +0530

    Add files via upload

---
+++ Arithmatic.java
@@ -0,0 +1,10 @@
+public class Arithmatic {
+    public static void main(String[] args) {
+        int num1=3,num2=2;
+        System.out.println(num1+num2);
+        System.out.println(num1-num2);
+        System.out.println(num1*num2);
+        System.out.println(num1/num2);
+        System.out.println(num1%num2);
+    }
+}
