daac9e1418c3452b9e552f95faed7cba2f04b9d5
Sankalp52 <143029767+Sankalp52@users.noreply.github.com>
2024-10-20 17:03:15 +0530

    Add files via upload

---
+++ Assingmentoperator.java
@@ -0,0 +1,9 @@
+public class Assingmentoperator {
+    public static void main(String[] args) {
+        int myint=3;
+        System.out.println(myint);
+        int yourint=myint;
+        System.out.println(yourint);
+
+    }
+}
